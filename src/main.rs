e8f5169c8a8a57c9f9e0b734f19f77de3cab2838
John Doe <paladinarr+gitlab@gmail.com>
2024-04-21 20:27:21 +0000

    Added a dichromacy mode, not sure I like it, need test subjects

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
-use std::{env::args, path::PathBuf};
+use std::path::PathBuf;
 
-use image::{codecs::png::PngEncoder, DynamicImage, GenericImageView, Rgb};
+use image::{codecs::png::PngEncoder, DynamicImage, GenericImageView, ImageBuffer, Rgb};
 
 fn main() {
-	let pattern_name = args().nth(1).expect("Missing pattern argument");
-	let image_path = PathBuf::from(args().nth(2).expect("Missing image argument"));
-	let output_path = args().nth(3).unwrap_or_else(|| {
-		format!(
-			"{pattern_name} - {}.png",
-			image_path
-				.file_stem()
-				.expect("Image path had no file name")
-				.to_str()
-				.unwrap()
-		)
-	});
-	let patterns = ChannelPatterns::load(&pattern_name);
-	let mut image = image::open(image_path).unwrap().into_rgb8();
-	for (x, y, colour) in image.enumerate_pixels_mut() {
-		*colour = patterns.apply(x, y, *colour);
-	}
+	let mut args = std::env::args();
+	let mode = match args.nth(1).expect("Missing chromacy argument").as_str() {
+		"1" => ColourBlindness::Monochromacy,
+		"2" => {
+			let missing_colour = Colour::parse(&args.next().expect("Missing colour argument"));
+			ColourBlindness::Dichromacy(missing_colour)
+		}
+		_ => panic!("Unexpected chromacy argument"),
+	};
+	let filter_name = args.next().expect("Missing filter argument");
+	let image_path = PathBuf::from(args.next().expect("Missing image argument"));
+	let image_file_stem = image_path
+		.file_stem()
+		.expect("Image path had no file name")
+		.to_str()
+		.unwrap();
+	let image = image::open(&image_path).unwrap().into_rgb8();
+	match mode {
+		ColourBlindness::Monochromacy => {
+			apply_monochromacy_filter(&filter_name, image, image_file_stem, args.next());
+		}
+		ColourBlindness::Dichromacy(missing_colour) => {
+			apply_dichromacy_filter(
+				&filter_name,
+				image,
+				image_file_stem,
+				args.next(),
+				missing_colour,
+			);
+		}
+	};
+}
+
+fn apply_monochromacy_filter(
+	filter_name: &str,
+	mut image: ImageBuffer<Rgb<u8>, Vec<u8>>,
+	image_file_stem: &str,
+	output_path: Option<String>,
+) {
+	let output_path =
+		output_path.unwrap_or_else(|| format!("{filter_name} - {image_file_stem}.png",));
+	let filter = MonochromacyFilter::load(filter_name);
+	filter.apply_image(&mut image);
 	DynamicImage::ImageRgb8(image)
 		.into_luma8()
 		.write_with_encoder(PngEncoder::new_with_quality(
@@ -30,17 +56,50 @@ fn main() {
 		.unwrap();
 }
 
-struct ChannelPatterns(DynamicImage);
+fn apply_dichromacy_filter(
+	filter_name: &str,
+	mut image: ImageBuffer<Rgb<u8>, Vec<u8>>,
+	image_file_stem: &str,
+	output_path: Option<String>,
+	missing_colour: Colour,
+) {
+	let output_path = output_path
+		.unwrap_or_else(|| format!("{filter_name} - {missing_colour} - {image_file_stem}.png",));
+	let filter = DichromacyFilter::load(filter_name, missing_colour);
+	filter.apply_image(&mut image);
+	DynamicImage::ImageRgb8(image)
+		.write_with_encoder(PngEncoder::new_with_quality(
+			std::fs::File::create(output_path).unwrap(),
+			image::codecs::png::CompressionType::Best,
+			image::codecs::png::FilterType::Adaptive,
+		))
+		.unwrap();
+}
 
-impl ChannelPatterns {
-	fn load(name: &str) -> Self {
-		let patterns = image::io::Reader::open(format!("patterns/{name}"))
+#[derive(Clone, Copy)]
+pub enum Colour {
+	Red,
+	Green,
+	Blue,
+}
+
+#[derive(Clone, Copy)]
+pub enum ColourBlindness {
+	Monochromacy,
+	Dichromacy(Colour),
+}
+
+pub struct MonochromacyFilter(DynamicImage);
+
+impl MonochromacyFilter {
+	pub fn load(name: &str) -> Self {
+		let filter = image::io::Reader::open(format!("filters/{name}"))
 			.unwrap()
 			.decode()
 			.unwrap();
-		Self(patterns)
+		Self(filter)
 	}
-	fn apply(&self, x: u32, y: u32, colour: Rgb<u8>) -> Rgb<u8> {
+	fn apply_pixel(&self, x: u32, y: u32, colour: Rgb<u8>) -> Rgb<u8> {
 		let pixel = self.0.get_pixel(x % self.0.width(), y % self.0.height());
 		let intensity = colour
 			.0
@@ -50,6 +109,79 @@ impl ChannelPatterns {
 			.max()
 			.unwrap();
 		Rgb([intensity, intensity, intensity])
+	}
+	pub fn apply_image(&self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
+		for (x, y, colour) in image.enumerate_pixels_mut() {
+			*colour = self.apply_pixel(x, y, *colour);
+		}
+	}
+}
+
+impl Colour {
+	pub fn parse(text: &str) -> Self {
+		match text {
+			"red" => Self::Red,
+			"green" => Self::Green,
+			"blue" => Self::Blue,
+			_ => panic!("Unexpected colour argument {text}"),
+		}
+	}
+}
+
+impl std::fmt::Display for Colour {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Red => f.write_str("red"),
+			Self::Green => f.write_str("green"),
+			Self::Blue => f.write_str("blue"),
+		}
+	}
+}
+
+pub struct DichromacyFilter {
+	filter: DynamicImage,
+	missing_colour: Colour,
+}
+
+impl DichromacyFilter {
+	pub fn load(name: &str, missing_colour: Colour) -> Self {
+		let filter = image::io::Reader::open(format!("filters/{name}"))
+			.unwrap()
+			.decode()
+			.unwrap();
+		Self {
+			filter,
+			missing_colour,
+		}
+	}
+	fn apply_pixel(&self, x: u32, y: u32, colour: Rgb<u8>) -> Rgb<u8> {
+		let pixel = self
+			.filter
+			.get_pixel(x % self.filter.width(), y % self.filter.height());
+		if pixel.0[0] == 0 {
+			Rgb([colour[self.missing_colour as usize]; 3])
+		} else {
+			colour
+			// Rgb(std::array::from_fn(|i| {
+			// 	if i == self.missing_colour as usize {
+			// 		(colour
+			// 			.0
+			// 			.iter()
+			// 			.enumerate()
+			// 			.filter_map(|(i, v)| {
+			// 				(i != self.missing_colour as usize).then_some(*v as u16)
+			// 			})
+			// 			.sum::<u16>() / 2) as u8
+			// 	} else {
+			// 		colour.0[i]
+			// 	}
+			// }))
+		}
+	}
+	pub fn apply_image(&self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
+		for (x, y, colour) in image.enumerate_pixels_mut() {
+			*colour = self.apply_pixel(x, y, *colour);
+		}
 	}
 }
 
